7008d12730b023058a47bf66cbe725032569c197
rayHrx <huang627158768@gmail.com>
2021-03-29 23:48:40 -0400

    -Integrate Gradle package management tool

--- src/app/App.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-class App {
-    public static void main(String[] args) {
-        System.out.println("Hello shit world!");
-    }
-}
---
+++ src/main/java/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+import com.owlike.genson.annotation.JsonProperty;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+class App {
+    public static void main(String[] args) {
+        System.out.println("Hello shit world!");
+    }
+}
