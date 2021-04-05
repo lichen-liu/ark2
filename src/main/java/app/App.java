e3d14de59e57345392ff3ee36190e08ad7b30eb1
rayHrx <huang627158768@gmail.com>
2021-04-04 23:21:35 -0400

    -Add scripts to create test hyper frabric network and chaincode

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,10 +2,6 @@ package app;
 
 import java.net.InetSocketAddress;
 
-import com.owlike.genson.annotation.JsonProperty;
-import org.hyperledger.fabric.contract.annotation.DataType;
-import org.hyperledger.fabric.contract.annotation.Property;
-
 class App {
     public static void main(String[] args) throws Exception {
         Conf conf = new Conf();
