3c672a5ca702717efa7480808d55e89761b8f371
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-08 23:52:02 -0400

    update application

--- application/src/main/java/app/App.java
+++ application/src/main/java/app/App.java
@@ -23,49 +23,50 @@ class App {
     private static void invokePeer() {
 
         try {
-			//EnrollAdmin.main(null);
-			//RegisterUser.main(null);
-		} catch (Exception e) {
+            EnrollAdmin.main(null);
+            RegisterUser.main(null);
+        } catch (Exception e) {
             System.out.println("fuck");
-			System.err.println(e);
-		}
+            System.err.println(e);
+        }
 
-		// connect to the network and invoke the smart contract
-		try (Gateway gateway = connect()) {
-			Network network = gateway.getNetwork("mychannel");
-			Contract contract = network.getContract("Agreements");
+        // connect to the network and invoke the smart contract
+        try (Gateway gateway = connect()) {
+            Network network = gateway.getNetwork("mychannel");
+            Contract contract = network.getContract("Agreements");
 
             try {
                 byte[] result = contract.evaluateTransaction("getPost", "POST1");
                 System.out.println("result: " + new String(result));
-            } catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace(System.out);
-            } 
+            }
         } catch (Exception e) {
             e.printStackTrace(System.out);
         }
     }
 
-    private static Gateway connect() throws Exception{
-		// Load a file system based wallet for managing identities.
-		Path walletPath = Paths.get("wallet");
-		Wallet wallet = Wallets.newFileSystemWallet(walletPath);
-		// load a CCP
-		Path networkConfigPath = Paths.get("..", "blockchain", "hlf2-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+    private static Gateway connect() throws Exception {
+        // Load a file system based wallet for managing identities.
+        Path walletPath = Paths.get("wallet");
+        Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+        // load a CCP
+        Path networkConfigPath = Paths.get("..", "blockchain", "hlf2-network", "organizations", "peerOrganizations",
+                "org1.example.com", "connection-org1.yaml");
 
         Gateway.Builder builder = Gateway.createBuilder();
-		builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
-		return builder.connect();
+        builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+        return builder.connect();
     }
-    
-//     public static PublicKey get(String filename)
-//     throws Exception {
 
-//     byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+    // public static PublicKey get(String filename)
+    // throws Exception {
 
-//     X509EncodedKeySpec spec =
-//       new X509EncodedKeySpec(keyBytes);
-//     KeyFactory kf = KeyFactory.getInstance("RSA");
-//     return kf.generatePublic(spec);
-//   }
+    // byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+
+    // X509EncodedKeySpec spec =
+    // new X509EncodedKeySpec(keyBytes);
+    // KeyFactory kf = KeyFactory.getInstance("RSA");
+    // return kf.generatePublic(spec);
+    // }
 }
