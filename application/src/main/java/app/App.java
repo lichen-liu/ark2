a6f5ed16c3016e747eda5a528ffe9583d0a70755
rayHrx <huang627158768@gmail.com>
2021-04-08 21:19:48 -0400

    -Update .gitignore and other stuff

--- application/src/main/java/app/App.java
+++ application/src/main/java/app/App.java
@@ -2,15 +2,56 @@ package app;
 
 import java.net.InetSocketAddress;
 
-import com.owlike.genson.annotation.JsonProperty;
-import org.hyperledger.fabric.contract.annotation.DataType;
-import org.hyperledger.fabric.contract.annotation.Property;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.hyperledger.fabric.gateway.Contract;
+import org.hyperledger.fabric.gateway.Gateway;
+import org.hyperledger.fabric.gateway.Network;
+import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric.gateway.Wallets;
 
 class App {
     public static void main(String[] args) throws Exception {
-        Conf conf = new Conf();
-        System.out.println(conf);
+        // Conf conf = new Conf();
+        // System.out.println(conf);
 
-        AppServer server = new AppServer(conf.getAppServerSocketAddress());
+        // AppServer server = new AppServer(conf.getAppServerSocketAddress());
+
+        invokePeer();
     }
+
+    private static void invokePeer() {
+
+        try {
+			//EnrollAdmin.main(null);
+			//RegisterUser.main(null);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+
+		// connect to the network and invoke the smart contract
+		try (Gateway gateway = connect()) {
+			Network network = gateway.getNetwork("mychannel");
+			Contract contract = network.getContract("Agreements");
+
+			byte[] result;
+
+			result = contract.evaluateTransaction("getPost", "POST1");
+            System.out.println("result: " + new String(result));
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    private static Gateway connect() throws Exception{
+		// Load a file system based wallet for managing identities.
+		Path walletPath = Paths.get("wallet");
+		Wallet wallet = Wallets.newFileSystemWallet(walletPath);
+		// load a CCP
+		Path networkConfigPath = Paths.get("..", "chaincode", "hlf2-network", "organizations", "peerOrganizations", "org1.example.com", "connection-org1.yaml");
+
+		Gateway.Builder builder = Gateway.createBuilder();
+		builder.identity(wallet, "appUser").networkConfig(networkConfigPath).discovery(true);
+		return builder.connect();
+	}
 }
