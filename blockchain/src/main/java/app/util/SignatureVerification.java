7f8a71609fd485c516614ce8c3418eec0dcfdb3d
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-10 23:41:59 -0400

    minor

--- blockchain/src/main/java/app/util/SignatureVerification.java
+++ blockchain/src/main/java/app/util/SignatureVerification.java
@@ -5,7 +5,7 @@ public interface SignatureVerification {
 
     public abstract String getSignatureString();
 
-    public abstract String getPublicKey();
+    public abstract String getPublicKeyString();
 
     public default boolean isMatchingSignature() {
         return true;
