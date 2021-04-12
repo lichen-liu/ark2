b91b3f2c74b7f609b14de352ea0374dfe7eb2774
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-12 19:32:35 -0400

    app cctesting point transaction submit and query

--- application/src/main/java/app/CCTesting.java
+++ application/src/main/java/app/CCTesting.java
@@ -20,11 +20,50 @@ public class CCTesting {
             final Contract contract = appPeer.getContract();
 
             print(appPeer.getContract().submitTransaction("publishNewPost", "a", "a", "a", "a"));
-            final String r0 = appPeer.publishNewPost("hahaha");
-            print(r0);
+
+            final String p0 = appPeer.publishNewPost("hahaha");
+            print(p0);
+            print(contract.evaluateTransaction("getPostByKey", p0));
+
             print(appPeer.fetchAllPosts());
-            print(appPeer.fetchAllPostKeys().toString());
-            print(contract.evaluateTransaction("getPostByKey", r0));
+
+            print(appPeer.fetchAllPostKeys());
+
+            final var t0 = toString(contract.submitTransaction("publishNewPointTransaction", "20210412_155300",
+                    "{\"userId\":\"bank\",\"pointAmount\":100}", "bank", "reference", "signature(bank)",
+                    "[{\"userId\":\"ray\",\"pointAmount\":100}]"));
+            print(t0);
+            print(contract.evaluateTransaction("getPointTransactionByKey", t0));
+
+            final var t1 = toString(contract.submitTransaction("publishNewPointTransaction", "20210412_155400",
+                    "{\"pointAmount\":100,\"userId\":\"ray\"}", "ray", "reference", "signature(ray)",
+                    "[{\"pointAmount\":50,\"userId\":\"charles\"},{\"pointAmount\":50,\"userId\":\"zac\"}]"));
+            print(t1);
+            print(contract.evaluateTransaction("getPointTransactionByKey", t1));
+
+            final var t2 = toString(contract.submitTransaction("publishNewPointTransaction", "20210412_155500",
+                    "{\"pointAmount\":100,\"userId\":\"bank\"}", "bank", "reference", "signature(bank)",
+                    "[{\"pointAmount\":100,\"userId\":\"ray\"}]"));
+            print(t2);
+            print(contract.evaluateTransaction("getPointTransactionByKey", t2));
+
+            final var t3 = toString(contract.submitTransaction("publishNewPointTransaction", "20210412_155600",
+                    "{\"pointAmount\":100,\"userId\":\"bank\"}", "bank", "reference", "signature(bank)",
+                    "[{\"pointAmount\":100,\"userId\":\"ray\"}]"));
+            print(t3);
+            print(contract.evaluateTransaction("getPointTransactionByKey", t3));
+
+            final var t4 = toString(contract.submitTransaction("publishNewPointTransaction", "20210412_155700",
+                    "{\"pointAmount\":150,\"userId\":\"ray\"}", "ray", "reference", "signature(ray)",
+                    "[{\"pointAmount\":50,\"userId\":\"charles\"},{\"pointAmount\":100,\"userId\":\"zac\"}]"));
+            print(t4);
+            print(contract.evaluateTransaction("getPointTransactionByKey", t4));
+
+            print("bank: " + new String(contract.evaluateTransaction("getPointAmountByUserId", "bank")));
+            print("ray: " + new String(contract.evaluateTransaction("getPointAmountByUserId", "ray")));
+            print("charles: " + new String(contract.evaluateTransaction("getPointAmountByUserId", "charles")));
+            print("zac: " + new String(contract.evaluateTransaction("getPointAmountByUserId", "zac")));
+            print(contract.evaluateTransaction("getAllPointTransactionKeys"));
         } catch (final Exception e) {
             e.printStackTrace();
         }
@@ -32,7 +71,7 @@ public class CCTesting {
     }
 
     private void print(final byte[] result) {
-        this.print(new String(result));
+        this.print(toString(result));
     }
 
     private void print(final String result) {
@@ -49,6 +88,10 @@ public class CCTesting {
         }
 
         this.testId++;
+    }
+
+    private static String toString(final byte[] result) {
+        return new String(result);
     }
 
     private String prettifyJson(String raw) {
