64a7eff3960d4186bc2a0e40d7c68422ba624fe1
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-12 21:35:20 -0400

    cc+app: remove reference from pt tx datatype

--- blockchain/src/main/java/app/datatype/PointTransaction.java
+++ blockchain/src/main/java/app/datatype/PointTransaction.java
@@ -27,12 +27,6 @@ public final class PointTransaction implements KeyGeneration, ComparableByTimest
     private final String issuerUserId;
 
     /**
-     * A user specified reference number, can be arbitrary
-     */
-    @Property
-    private final String reference;
-
-    /**
      * sign(privateKey, hash(timestamp, payerEntry, issuerUserId, reference))
      */
     @Property
@@ -120,10 +114,6 @@ public final class PointTransaction implements KeyGeneration, ComparableByTimest
         return issuerUserId;
     }
 
-    public String getReference() {
-        return reference;
-    }
-
     public String getSignature() {
         return signature;
     }
@@ -143,14 +133,12 @@ public final class PointTransaction implements KeyGeneration, ComparableByTimest
 
     public PointTransaction(@JsonProperty("timestamp") final String timestamp,
             @JsonProperty("payerEntry") final Entry payerEntry, @JsonProperty("issuerUserId") final String issuerUserId,
-            @JsonProperty("reference") final String reference, @JsonProperty("signature") final String signature,
-            @JsonProperty("payeeEntries") final Entry[] payeeEntries,
+            @JsonProperty("signature") final String signature, @JsonProperty("payeeEntries") final Entry[] payeeEntries,
             @JsonProperty("relativeOrder") final long relativeOrder,
             @JsonProperty("payerPointTransactionTracking") final Tracking payerPointTransactionTracking) {
         this.timestamp = timestamp;
         this.payerEntry = payerEntry;
         this.issuerUserId = issuerUserId;
-        this.reference = reference;
         this.signature = signature;
         this.payeeEntries = payeeEntries;
         this.relativeOrder = relativeOrder;
