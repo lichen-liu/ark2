4ec4be1c0d5e1cf19a1a9545ce27646f06b5095c
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-13 20:29:54 -0400

    c likerewarding policy

---
+++ blockchain/src/main/java/app/policy/LikeRewarding.java
@@ -0,0 +1,13 @@
+package app.policy;
+
+public class LikeRewarding {
+    private final long numberLikes;
+
+    public long getNumberLikes() {
+        return numberLikes;
+    }
+
+    public LikeRewarding(long numberLikes) {
+        this.numberLikes = numberLikes;
+    }
+}
