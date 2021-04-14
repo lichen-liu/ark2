1f31b98eb6f1b60b7db338d6f519f5180405c588
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-13 21:35:42 -0400

    cc determineauthorrewarding

--- blockchain/src/main/java/app/policy/LikeRewarding.java
+++ blockchain/src/main/java/app/policy/LikeRewarding.java
@@ -2,12 +2,26 @@ package app.policy;
 
 public class LikeRewarding {
     private final long numberLikes;
+    private final double basePointAmount;
+
+    private static final double splitToAuthorRatio = 0.5;
+    private static final double inflationRate = 0.02;
 
     public long getNumberLikes() {
         return numberLikes;
     }
 
-    public LikeRewarding(long numberLikes) {
-        this.numberLikes = numberLikes;
+    public double getBasePointAmount() {
+        return basePointAmount;
     }
+
+    public LikeRewarding(long numberLikes, double basePointAmount) {
+        this.numberLikes = numberLikes;
+        this.basePointAmount = basePointAmount;
+    }
+
+    public double determineAuthorRewarding() {
+        return this.basePointAmount * (splitToAuthorRatio + inflationRate * Math.log(this.numberLikes));
+    }
+
 }
