3af82b7e79c217070ffaa8fdfac5bf19c6922232
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-16 01:29:19 -0400

    app cryptography minor

--- application/src/main/java/app/utils/Cryptography.java
+++ application/src/main/java/app/utils/Cryptography.java
@@ -1,15 +1,11 @@
 package app.utils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
@@ -19,49 +15,17 @@ import java.security.spec.ECPoint;
 import java.security.spec.ECPrivateKeySpec;
 import java.security.spec.ECPublicKeySpec;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 
 import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.ECPointUtil;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
 import org.bouncycastle.jce.spec.ECNamedCurveSpec;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
 
 public class Cryptography {
 
     static {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-    }
-
-    private PemObject pemObject;
-    private final KeyFactory factory;
-
-    public Cryptography(final String filename, final String algorithm, final String provider)
-            throws IOException, NoSuchAlgorithmException, NoSuchProviderException {
-
-        final PemReader pemReader = new PemReader(new InputStreamReader(new FileInputStream(filename)));
-        try {
-            this.pemObject = pemReader.readPemObject();
-        } finally {
-            pemReader.close();
-        }
-
-        this.factory = KeyFactory.getInstance(algorithm, provider);
-    }
-
-    public PublicKey getPublicKey() throws InvalidKeySpecException {
-        final byte[] content = pemObject.getContent();
-        final X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(content);
-        return factory.generatePublic(pubKeySpec);
-    }
-
-    public PrivateKey getPrivateKey() throws InvalidKeySpecException {
-        final byte[] content = pemObject.getContent();
-        final PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(content);
-        return factory.generatePrivate(privKeySpec);
     }
 
     public static PrivateKey parsePrivateKey(final byte[] keyBin)
