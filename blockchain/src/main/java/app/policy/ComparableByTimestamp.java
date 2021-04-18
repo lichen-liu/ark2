28b342dd1baa79c02da7c5277f80f133d930f3b8
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-18 00:29:27 -0400

    cc comparablebytimestamp

--- blockchain/src/main/java/app/policy/ComparableByTimestamp.java
+++ blockchain/src/main/java/app/policy/ComparableByTimestamp.java
@@ -1,5 +1,9 @@
 package app.policy;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+
 public interface ComparableByTimestamp {
     /**
      * 
@@ -12,6 +16,20 @@ public interface ComparableByTimestamp {
     public abstract String getTimestamp();
 
     public default int compareToByTimestamp(final ComparableByTimestamp other) {
-        return getTimestamp().compareToIgnoreCase(other.getTimestamp());
+        ZonedDateTime myInstant;
+        try {
+            myInstant = ZonedDateTime.parse(this.getTimestamp());
+        } catch (final DateTimeParseException e) {
+            myInstant = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+        }
+
+        ZonedDateTime otherInstant;
+        try {
+            otherInstant = ZonedDateTime.parse(other.getTimestamp());
+        } catch (final DateTimeParseException e) {
+            otherInstant = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+        }
+
+        return myInstant.compareTo(otherInstant);
     }
 }
