e3f634020d224f5063877985320b46969b48c5e1
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-04-21 03:07:20 -0400

    app gui point balance tab

--- application/src/main/java/app/gui/PointHistoryChartPanel.java
+++ application/src/main/java/app/gui/PointHistoryChartPanel.java
@@ -15,6 +15,7 @@ import org.jfree.data.general.DatasetUtils;
 
 public class PointHistoryChartPanel extends ChartPanel {
     private CategoryDataset pointBalanceHistoryDataset = null;
+    private CategoryDataset pointBalanceChangesHistorydataset = null;
 
     public PointHistoryChartPanel() {
         super(null);
@@ -23,19 +24,35 @@ public class PointHistoryChartPanel extends ChartPanel {
         super.setMouseWheelEnabled(true);
     }
 
-    public void setDataModel(final double[] pointBalanceHistory) {
-        this.pointBalanceHistoryDataset = DatasetUtils.createCategoryDataset(
-                new String[] { "Point Balance By Transaction Order" },
-                IntStream.rangeClosed(0, pointBalanceHistory.length - 1).boxed().collect(Collectors.toList())
-                        .toArray(Integer[]::new),
-                new double[][] { pointBalanceHistory });
+    public void setDataModel(final double[] pointBalanceHistory, final double[] pointBalanceChangesHistory) {
+        this.pointBalanceHistoryDataset = DatasetUtils
+                .createCategoryDataset(
+                        new String[] { "Point Balance" }, IntStream.rangeClosed(0, pointBalanceHistory.length - 1)
+                                .boxed().collect(Collectors.toList()).toArray(Integer[]::new),
+                        new double[][] { pointBalanceHistory });
+
+        this.pointBalanceChangesHistorydataset = DatasetUtils
+                .createCategoryDataset(new String[] { "Point Balance Changes" },
+                        IntStream.rangeClosed(0, pointBalanceChangesHistory.length - 1).boxed()
+                                .collect(Collectors.toList()).toArray(Integer[]::new),
+                        new double[][] { pointBalanceChangesHistory });
     }
 
     public void displayPointBalanceHistory() {
-        super.setChart(null);
-
         final JFreeChart chart = ChartFactory.createLineChart("Point Balance By Transaction Order", "Transaction",
                 "Point Balance", this.pointBalanceHistoryDataset, PlotOrientation.VERTICAL, true, true, false);
+        this.displayLineChart(chart);
+    }
+
+    public void displayPointBalanceChangesHistory() {
+        final JFreeChart chart = ChartFactory.createLineChart("Point Balance Changes By Transaction Order",
+                "Transaction", "Point Balance Changes", this.pointBalanceChangesHistorydataset,
+                PlotOrientation.VERTICAL, true, true, false);
+        this.displayLineChart(chart);
+    }
+
+    private void displayLineChart(final JFreeChart chart) {
+        super.setChart(null);
         chart.getLegend().setFrame(BlockBorder.NONE);
 
         final LineAndShapeRenderer renderer = new LineAndShapeRenderer();
@@ -52,5 +69,4 @@ public class PointHistoryChartPanel extends ChartPanel {
 
         super.setChart(chart);
     }
-
 }
