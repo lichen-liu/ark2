6267820ccbd9ca70d080ec59fab7f6e5425991eb
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-05-04 18:56:04 -0400

    app add stopwatch to testsuite

--- application/src/main/java/app/tests/TestSuite.java
+++ application/src/main/java/app/tests/TestSuite.java
@@ -1,10 +1,14 @@
 package app.tests;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.base.Stopwatch;
 
 import app.tests.util.Logger;
 
 public abstract class TestSuite {
+    private final String suiteName;
     private final Logger.Builder loggerBuilder;
 
     protected TestSuite() {
@@ -12,15 +16,27 @@ public abstract class TestSuite {
     }
 
     protected TestSuite(final String suiteName) {
+        this.suiteName = suiteName;
         this.loggerBuilder = new Logger.Builder(suiteName);
     }
 
-    public final void launchTests() {
+    public void launchTests() {
+        launchTestsImplementation();
+    }
+
+    protected final void launchTestsImplementation() {
         final var tests = setUpTests();
         for (final var test : tests) {
             final Logger logger = test.initLogger(loggerBuilder);
             test.preTest(logger);
+
+            final Stopwatch timer = Stopwatch.createStarted();
+
             test.runTest(logger);
+
+            timer.stop();
+            System.out.println(suiteName + ": " + timer.elapsed(TimeUnit.MILLISECONDS));
+
             test.postTest(logger);
         }
     }
