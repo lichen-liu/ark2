62ced89da36225463493408101057fd394cc2b93
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-05-04 20:00:21 -0400

    app tests performance

--- application/src/main/java/app/tests/performance/DislikePublishingTests.java
+++ application/src/main/java/app/tests/performance/DislikePublishingTests.java
@@ -1,5 +1,20 @@
 package app.tests.performance;
 
-public class DislikePublishingTests {
-    
+import app.tests.Test;
+import app.tests.util.Logger;
+import app.tests.util.Logger.Builder;
+
+public class DislikePublishingTests implements Test {
+
+    @Override
+    public Logger initLogger(final Builder builder) {
+        return builder.create("DislikePublishingTests");
+    }
+
+    @Override
+    public void runTest(final Logger logger) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
--- application/src/main/java/app/tests/performance/LikePublishingTests.java
+++ application/src/main/java/app/tests/performance/LikePublishingTests.java
@@ -1,5 +1,20 @@
 package app.tests.performance;
 
-public class LikePublishingTests {
-    
+import app.tests.Test;
+import app.tests.util.Logger;
+import app.tests.util.Logger.Builder;
+
+public class LikePublishingTests implements Test {
+
+    @Override
+    public Logger initLogger(final Builder builder) {
+        return builder.create("LikePublishingTests");
+    }
+
+    @Override
+    public void runTest(final Logger logger) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
