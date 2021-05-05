33a75ce6ca138de2518aa3e917315b243fe4241e
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-05-05 04:02:25 -0400

    add various performance tests for todo

---
+++ application/src/main/java/app/tests/performance/LikesFetchingTests.java
@@ -0,0 +1,19 @@
+package app.tests.performance;
+
+import app.tests.Testable;
+import app.tests.util.Logger;
+
+public class LikesFetchingTests implements Testable {
+
+    @Override
+    public String testName() {
+        return "LikesFetchingTests";
+    }
+
+    @Override
+    public boolean runTest(final Logger logger, final int currentIteration) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
---
+++ application/src/main/java/app/tests/performance/PointTransactionsFetchingTests.java
@@ -0,0 +1,19 @@
+package app.tests.performance;
+
+import app.tests.Testable;
+import app.tests.util.Logger;
+
+public class PointTransactionsFetchingTests implements Testable {
+
+    @Override
+    public String testName() {
+        return "PointTransactionsFetchingTests";
+    }
+
+    @Override
+    public boolean runTest(final Logger logger, final int currentIteration) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
