2c5f01074fb9413432bf27a1b012222a18d0ac32
rayHrx <huang627158768@gmail.com>
2021-05-04 21:21:28 -0400

    -Add performance tests

--- application/src/main/java/app/tests/performance/DislikePublishingTests.java
+++ application/src/main/java/app/tests/performance/DislikePublishingTests.java
@@ -1,9 +1,37 @@
 package app.tests.performance;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+
+import org.hyperledger.fabric.gateway.Contract;
+
 import app.tests.Test;
 import app.tests.util.Logger;
+import app.tests.util.TestClient;
+import app.user.NamedService;
 
 public class DislikePublishingTests implements Test {
+
+    private final Contract contract;
+    private String postKey;
+    private NamedService user = null;
+
+    public DislikePublishingTests(final Contract contract) {
+        this.contract = contract;
+    }
+
+    @Override
+    public boolean preTest(final Logger logger, final int currentIteration) {
+        try {
+            this.user = TestClient.createTestClient(contract);
+            this.postKey = user.publishNewPost("_");
+            
+        } catch (InvalidAlgorithmParameterException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public String testName() {
@@ -12,7 +40,11 @@ public class DislikePublishingTests implements Test {
 
     @Override
     public boolean runTest(final Logger logger, final int currentIteration) {
+
+        postKey = this.user.publishNewDislike(postKey);
+        logger.print(postKey);
         return true;
+
     }
 
 }
--- application/src/main/java/app/tests/performance/LikePublishingTests.java
+++ application/src/main/java/app/tests/performance/LikePublishingTests.java
@@ -1,19 +1,49 @@
 package app.tests.performance;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+
+import org.hyperledger.fabric.gateway.Contract;
+
 import app.tests.Test;
 import app.tests.util.Logger;
+import app.tests.util.TestClient;
+import app.user.NamedService;
 
 public class LikePublishingTests implements Test {
+
+    private final Contract contract;
+    private String postKey;
+    private NamedService user = null;
 
     @Override
     public String testName() {
         return "LikePublishingTests";
     }
 
+    public LikePublishingTests(final Contract contract) {
+        this.contract = contract;
+    }
+
+    @Override
+    public boolean preTest(final Logger logger, final int currentIteration) {
+        try {
+            this.user = TestClient.createTestClient(contract);
+            this.postKey = user.publishNewPost("_");
+            
+        } catch (InvalidAlgorithmParameterException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public boolean runTest(final Logger logger, final int currentIteration) {
+      
+        postKey = this.user.publishNewLike(postKey);
+        logger.print(postKey);
         return true;
-        // TODO Auto-generated method stub
 
     }
 
