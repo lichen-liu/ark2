f5061c38298ec32e9749cc5085c5037c9bc9b6b3
Lichen Liu <issac.liu@mail.utoronto.ca>
2021-05-07 18:27:33 -0400

    minor scripts

--- application/analyzer/__init__.py
+++ application/analyzer/__init__.py
@@ -1,6 +1,6 @@
 import argparse
 
-from analyzer import performance
+from analyzer import performance, simulation
 
 
 def init(parser):
@@ -9,7 +9,12 @@ def init(parser):
     parser_performance = subparsers.add_parser('performance')
     performance.init(parser_performance)
 
+    parser_simulation = subparsers.add_parser('simulation')
+    simulation.init(parser_simulation)
+
 
 def main(args):
     if args.target == 'performance':
         performance.main(args)
+    elif args.target == 'simulation':
+        simulation.main(args)
